dbf35656712364db36ca8983dc5444b3ad04ce0d
Scott <55382673+7755919@users.noreply.github.com>
2024-05-30 13:43:10 +0800

    Update test_secrets.py

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -1,11 +1,11 @@
 import os
 
 # 从环境变量中获取 Secrets
-telegram_bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
-telegram_chat_id = os.environ.get("TELEGRAM_CHAT_ID")
-weibo_id = os.environ.get("WEIBO_ID")
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+WEIBO_ID = os.environ.get("WEIBO_ID")
 
 # 打印获取到的 Secrets
-print("Telegram Bot Token:", telegram_bot_token)
-print("Telegram Chat ID:", telegram_chat_id)
-print("Weibo ID:", weibo_id)
+print("TELEGRAM_BOT_TOKEN:", TELEGRAM_BOT_TOKEN)
+print("TELEGRAM_CHAT_ID:", TELEGRAM_CHAT_ID)
+print("WEIBO_ID:", WEIBO_ID)
