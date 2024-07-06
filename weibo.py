2e9f3eb305f0b5bd1681c1c7c5911324b8e6d0f7
Scott <55382673+7755919@users.noreply.github.com>
2024-07-06 19:23:28 +0800

    Update weibo.py

--- weibo.py
+++ weibo.py
@@ -8,23 +8,10 @@ import configparser
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 
-
 class Weibo:
-
-    def plog(self, content):
-        print('{} {}'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())), content))
-
-    def Load_conf():
-        WEIBO_ID = os.environ.get("WEIBO_ID")
-        TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-        TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
-
-
     def __init__(self):
         self.BASE_DIR = os.path.split(os.path.realpath(__file__))[0]
-        # Ensure the 'db' directory exists
         os.makedirs(os.path.join(self.BASE_DIR, 'db'), exist_ok=True)
-        # Check out the SQLite database file from the repository
         db_file_path = os.path.join(self.BASE_DIR, 'db', 'weibo.db')
         if not os.path.exists(db_file_path):
             print("SQLite database file does not exist. Checking out from repository...")
@@ -32,39 +19,26 @@ class Weibo:
             os.makedirs(os.path.dirname(db_file_path), exist_ok=True)
             os.system(f"cp /tmp/repository/db/weibo.db {db_file_path}")
 
-        # Get secrets from environment variables
         config = configparser.ConfigParser()
         config.read(os.path.join(self.BASE_DIR, 'config.ini'), encoding='utf-8')
         
-        proxy = config.get("CONFIG", "PROXY")
-        PROXIES = {"http": proxy, "https": proxy}
-
+        self.proxy = config.get("CONFIG", "PROXY")
+        self.PROXIES = {"http": self.proxy, "https": self.proxy}
         
-        #WEIBO_ID = os.environ.get("WEIBO_ID")
-        #TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-        #TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
-
+        self.WEIBO_ID = os.environ.get("WEIBO_ID")
+        self.TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+        self.TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
         self.SESSION = HTMLSession()
-        self.SESSION.adapters.DEFAULT_RETRIES = 5  # Increase retry attempts
-        self.SESSION.keep_alive = False  # Close additional connections
+        self.SESSION.adapters.DEFAULT_RETRIES = 5
+        self.SESSION.keep_alive = False
 
-
-        # Debugging lines to check if environment variables are loaded correctly
-        #print(f"WEIBO_ID: {self.WEIBO_ID}")
-        #print(f"TELEGRAM_BOT_TOKEN: {self.TELEGRAM_BOT_TOKEN}")
-        #print(f"TELEGRAM_CHAT_ID: {self.TELEGRAM_CHAT_ID}")
-        #print(f"PROXY: {proxy}")
+    def plog(self, content):
+        print('{} {}'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())), content))
 
     def send_telegram_message(self, text, weibo_link):
-        """
-        給TG發送文字消息
-        """
-        headers = {
-            'Content-Type': 'application/json',
-        }
-        data = f'{{"chat_id":"{self.TELEGRAM_CHAT_ID}", "text":"{text}", "reply_markup": {{"inline_keyboard":' \
-               f' [[{{"text":"🔗點擊查看原微博", "url":"{weibo_link}"}}]]}}}} '
+        headers = {'Content-Type': 'application/json'}
+        data = f'{{"chat_id":"{self.TELEGRAM_CHAT_ID}", "text":"{text}", "reply_markup": {{"inline_keyboard":[[{{"text":"🔗點擊查看原微博", "url":"{weibo_link}"}}]]}}}}'
         url = f'https://api.telegram.org/bot{self.TELEGRAM_BOT_TOKEN}/sendMessage'
         try:
             self.SESSION.post(url, headers=headers, data=data.encode('utf-8'), proxies=self.PROXIES)
@@ -73,39 +47,27 @@ class Weibo:
             time.sleep(99999)
 
     def send_telegram_photo(self, img_url):
-        """
-        給TG發送圖片
-        """
         url = f'https://api.telegram.org/bot{self.TELEGRAM_BOT_TOKEN}/sendPhoto'
-        data = dict(chat_id=f"{self.TELEGRAM_CHAT_ID}&", photo=img_url)
-
+        data = {'chat_id': self.TELEGRAM_CHAT_ID, 'photo': img_url}
         self.SESSION.post(url, data=data, proxies=self.PROXIES)
 
     def send_telegram_photos(self, pics):
         url = f'https://api.telegram.org/bot{self.TELEGRAM_BOT_TOKEN}/sendMediaGroup'
-        params = {
-            'chat_id': self.TELEGRAM_CHAT_ID,
-            'media': [],
-        }
+        params = {'chat_id': self.TELEGRAM_CHAT_ID, 'media': []}
         for pic in pics:
             params['media'].append({'type': 'photo', 'media': pic})
         params['media'] = json.dumps(params['media'])
         result = self.SESSION.post(url, data=params, proxies=self.PROXIES)
-        if result.status_code != 200:  # 如果分組發送失敗，則單獨發送圖片
+        if result.status_code != 200:
             for pic in pics:
                 self.send_telegram_photo(pic)
 
     def parse_weibo(self, weibo):
-        """
-        檢查當前微博是否已處理過，如果沒處理過則發送博文以及配圖到Telegram
-        """
         conn = sqlite3.connect(os.path.join(self.BASE_DIR, 'db', 'weibo.db'))
         cursor = conn.cursor()
-
         sql = "SELECT COUNT(id) AS counts FROM weibo WHERE link = ?"
         cursor.execute(sql, (weibo['link'],))
         result = cursor.fetchone()
-
         if result[0] <= 0:
             self.send_telegram_message(
                 '{}@{}:{}'.format(
@@ -115,33 +77,24 @@ class Weibo:
                 ),
                 weibo['link']
             )
-
-            # 把圖片url發送到Telegram中，可以第一時間在Telegram中收到推送
             pics = weibo['pics']
             if len(pics) > 0:
-                if len(pics) <= 2:  # 如果配圖小於2張，則一張一張獨立發送
+                if len(pics) <= 2:
                     for pic in pics:
-                        self.send_telegram_photo(pics)
-                elif len(pics) > 10:  # 如果配圖大於10張，則分2組發送
+                        self.send_telegram_photo(pic)
+                elif len(pics) > 10:
                     self.send_telegram_photos(pics[0: int(len(pics) / 2)])
                     self.send_telegram_photos(pics[int(len(pics) / 2):])
                 else:
                     self.send_telegram_photos(pics)
-
-            # 配圖發送到Telegram畢後，將配圖獨立保存到本地一份
             for pic in weibo['pics']:
                 filename = pic.split('/')[-1].split('?')[0]
                 filename = os.path.join(self.BASE_DIR, 'images', filename)
                 wget.download(pic, out=filename)
-
             sql = "INSERT INTO weibo(summary, link) VALUES(?, ?)"
-            cursor.execute(sql, (
-                weibo['title'],
-                weibo['link'],
-            ))
+            cursor.execute(sql, (weibo['title'], weibo['link']))
             conn.commit()
             conn.close()
-
             return True
         else:
             return False
@@ -154,7 +107,6 @@ class Weibo:
             print(f'【正確】當前設置的微博賬戶為：@{weibo_name}')
         except:
             print('【錯誤】請重新測試或檢查微博數字ID是否正確')
-
         print('\n* 正在檢查代理是否配置正確')
         try:
             status_code = self.SESSION.get('https://www.google.com', proxies=self.PROXIES, timeout=5).status_code
@@ -173,16 +125,13 @@ class Weibo:
         weibo['nickname'] = detail['data']['user']['screen_name']
         weibo_id = detail['data']['user']['id']
         weibo['pics'] = []
-        if 'pics' in detail['data']:  # 判斷博文中是否有配圖，如果有配圖則做解析
+        if 'pics' in detail['data']:
             weibo['pics'] = [pic['large']['url'] for pic in detail['data']['pics']]
         weibo['link'] = self.get_pc_url(weibo_id, bid)
         self.parse_weibo(weibo)
 
     def get_pc_url(self, weibo_id, bid):
-        return 'https://weibo.com/{weibo_id}/{uri}'.format(
-            weibo_id=weibo_id,
-            uri=bid
-        )
+        return f'https://weibo.com/{weibo_id}/{bid}'
 
     def run(self):
         self.plog('開始運行>>>')
@@ -190,57 +139,45 @@ class Weibo:
         while True:
             current_time = time.time()
             elapsed_time = current_time - start_time
-            if elapsed_time >= 86400:  # 如果已經運行了24小時，則退出循環
+            if elapsed_time >= 86400:
                 break
             self.plog(f'--- 第 {int(elapsed_time)} 秒 ---')
-
-            WEIBO_ID, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID = Weibo.load_conf()
-            weibo_ids = WEIBO_ID.split(',')
+            weibo_ids = self.WEIBO_ID.split(',')
             for weibo_id in weibo_ids:
                 self.plog(f'    |-開始獲取 {weibo_id} 的微博')
                 url = f'https://m.weibo.cn/api/container/getIndex?containerid=107603{weibo_id}'
-
                 try:
                     weibo_items = self.SESSION.get(url).json()['data']['cards'][::-1]
                 except:
                     self.plog('    |-訪問url出錯了')
-
                 for item in weibo_items:
                     weibo = {}
                     try:
-                        if item['mblog']['isLongText']:  # 如果博文包含全文 則去解析完整微博
+                        if item['mblog']['isLongText']:
                             self.get_weibo_detail(item['mblog']['bid'])
                             continue
                     except:
                         continue
-
                     weibo['title'] = BeautifulSoup(item['mblog']['text'].replace('<br />', '\n'), 'html.parser').get_text()
                     weibo['nickname'] = item['mblog']['user']['screen_name']
-
-                    if item['mblog'].get('weibo_position') == 3:  # 如果狀態為3表示轉發微博，附加上轉發鏈，狀態1為原創微博
+                    if item['mblog'].get('weibo_position') == 3:
                         retweet = item['mblog']['retweeted_status']
                         try:
                             weibo['title'] = f"{weibo['title']}//@{retweet['user']['screen_name']}:{retweet['raw_text']}"
                         except:
                             weibo['title'] = f"{weibo['title']}//轉發原文不可見，可能已被刪除"
-
                     try:
                         weibo['pics'] = [pic['large']['url'] for pic in item['mblog']['pics']]
                     except:
                         weibo['pics'] = []
-
                     weibo['link'] = self.get_pc_url(weibo_id, item['mblog']['bid'])
-
                     self.parse_weibo(weibo)
                 self.plog(f'    |-獲取結束 {weibo_id} 的微博')
-            time.sleep(10)  # 每秒循環一次
-        self.plog('<<<運行結束\n')
+            self.plog('>>>檢查更新結束')
+            self.plog('>>>180 秒後再次檢查')
+            time.sleep(180)
+        self.plog('運行結束>>>')
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     weibo = Weibo()
-    argv = sys.argv[1] if len(sys.argv) > 1 else ''
-    if argv.lower() == 'test':
-        weibo.test()
-    else:
-        weibo.run()
+    weibo.run()
